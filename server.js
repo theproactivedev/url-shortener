b25711367ce6c366b69a0a0c8366b7abb2deeb23
System <none>
2017-08-18 12:02:39 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -51,48 +51,16 @@ app.route('/')
 app.route("/new/:urlToShorten(*)")
   .get(function(req, res) {
   
-  var output = {};
-  var shortenedURL = "";
-  var url = req.params.urlToShorten;
+  var dbLink = "mongodb://admin_eirin:$h0rtur|@ds01316.mlab.com:1316/shorturl";
   
-  // Checks to see if it is an actual url
-  // Regex from https://gist.github.com/dperini/729294
-  var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-    
-  
-  if (regex.test(url)) {
-    var short = Math.floor(Math.random() * 100000).toString();
-    shortenedURL = short;
-    
-//     var data = new shortURL({
-//       originalURL : url,
-//       shortURL : shortenedURL
-//     });
-
-//     data.save(function(err) {
-//       if (err) return res.send("Error saving your data.");
-//     });
-    
-//     output = data;
-    
-  } else {
-    // shortURL = "Invalid URL";
-    // output = {
-    //   originalURL : url,
-    //   shortURL : shortURL
-    // }
-  }
-  
-  
-  var dbLink = "mongodb://username:password@ds01316.mlab.com:1316/shorturl";
-  
-  MongoClient.connect(, function (err, db) {
+  MongoClient.connect(dbLink, function (err, db) {
     if (err) {
       console.log('Unable to connect to the mongoDB server. Error:', err);
     } else {
       console.log('Connection established to', url);
 
       // do some work here with the database.
+      
 
       //Close connection
       db.close();
